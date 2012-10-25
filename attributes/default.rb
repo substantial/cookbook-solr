08097de007d42b791496722dab491eac7434fdb2
Aaron Jensen <aaronjensen@gmail.com>
2012-10-25 12:32:37 -0700

    Update to 3.6.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,7 +8,7 @@
 #
 
 
-default[:solr][:version] = "3.4.0"
+default[:solr][:version] = "3.6.1"
 default[:solr][:filename] = "apache-solr-#{node[:solr][:version]}.tgz"
 default[:solr][:download_url] = "http://apache.cs.utah.edu//lucene/solr/#{node[:solr][:version]}/#{node[:solr][:filename]}"
 default[:solr][:path] = "/var/local/solr"
