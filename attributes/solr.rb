7862988deac2cc2f415c036b0fd4be60351c9679
Aaron Jensen <aaronjensen@gmail.com>
2012-10-25 22:18:08 -0700

    Remove combined attributes

--- attributes/solr.rb
+++ attributes/solr.rb
@@ -9,6 +9,4 @@
 
 
 default[:solr][:version] = "3.6.1"
-default[:solr][:filename] = "apache-solr-#{node[:solr][:version]}.tgz"
-default[:solr][:download_url] = "http://apache.cs.utah.edu//lucene/solr/#{node[:solr][:version]}/#{node[:solr][:filename]}"
 default[:solr][:path] = "/var/local/solr"
